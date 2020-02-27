b5728c1c7a1b420174a46ed61d31ad2d0187afff
alber sánchez <albhasan@gmail.com>
2020-02-27 17:18:15 -0300

    Update remove_file_extension.R

--- R/remove_file_extension.R
+++ R/remove_file_extension.R
@@ -1,24 +1,15 @@
 #' Strips a file extension from a filename.
 #' @title remove_file_extension
 #' @param filename Character. The input filename.
-#' @param extension_delimiter Character. The extension or extension delimiter (default ".") to remove.
+#' @param compression Logical. Should compression extension .gz, .bz2 or .xz be removed first?
 #' @author Jonathan A. Greenberg \email{spatial.tools@@estarcion.net}
 #' @examples
 #' myfilename="my.file.gri"
-#' remove_file_extension(myfilename,".")
-#' remove_file_extension(myfilename,".file.gri")
+#' remove_file_extension(myfilename)
 #' @export
 
 
-remove_file_extension=function(filename,extension_delimiter=".")
+remove_file_extension=function(filename,compression = FALSE)
 {
-	split_filename=unlist(strsplit(filename,extension_delimiter,fixed=TRUE))
-	split_filename_length=length(split_filename)
-	if(split_filename_length==1)
-	{
-		return(split_filename[1])
-	} else
-	{
-		return(paste(as.character(split_filename)[1:(split_filename_length-1)],collapse=extension_delimiter))	
-	}
+    tools::file_path_sans_ext(filename, compression = compression)
 }
